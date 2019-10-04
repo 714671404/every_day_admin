18f7ea692d7ffdb8e1297165b2a26704c62b24cb
714671404 <714671404@qq.com>
2019-10-05 02:46:58 +0800

    登录é¡auth page get on

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,8 +1,87 @@
 <template>
-	<div class="auth">111</div>
+	<div class="auth">
+		<div class="auth-content">
+			<div class="title">every day</div>
+			<div class="form">
+				<form @submit.prevent="login">
+					<div class="">
+						<input type="test" v-model="form.username" placeholder="请输账号">
+					</div>
+					<div>
+						<input type="password" v-model="form.password" placeholder="请输密码">
+					</div>
+					<div>
+						<button type="submit" class="btn btn-blue">提交</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
 	export default {
-		name: 'login'
+		name: 'login',
+		data: () => ({
+			form: {
+				username: '',
+				password: ''
+			},
+			whitelist: {
+				phone: /^1\d{10}$/, // 验证手机号码简单正则
+				user: []
+			},
+			passwordLength: 6,
+			passwordLevel: [
+				'密码长度不能小于6',
+				'低等安全',
+				'中等安全',
+				'高等安全'
+			]
+		}),
+		computed: {
+			AnalyzePasswordSecurityLevel() { // 分析密码安全
+				let password = this.form.password;
+				let securityLevelFlag = 0; // 密码安全等级
+				if (password.length < this.passwordLength) { // 密码长度小于规定长度直接返回0
+					return 0;
+				}
+				else {
+					let securityLevelFlagArray = new Array(0, 0, 0, 0);
+					for (let i = 0; i < password.length; i++) {
+						let asciiNumber = password.substr(i, 1).charCodeAt();
+						if (asciiNumber >= 48 && asciiNumber <= 57) {
+							securityLevelFlagArray[0] = 1;  //digital
+						}
+						else if (asciiNumber >= 97 && asciiNumber <= 122) {
+							securityLevelFlagArray[1] = 1;  //lowercase
+						}
+						else if (asciiNumber >= 65 && asciiNumber <= 90) {
+							securityLevelFlagArray[2] = 1;  //uppercase
+						}
+						else {
+							securityLevelFlagArray[3] = 1;  //specialcase
+						}
+					}
+
+					for (let i = 0; i < securityLevelFlagArray.length; i++) {
+						if (securityLevelFlagArray[i] == 1) {
+							securityLevelFlag++;
+						}
+					}
+					return securityLevelFlag;
+				}
+			}
+		},
+		methods: {
+			login() {
+
+			},
+			verification(values) {
+				// 验证字符串
+				if (values) {
+
+				}
+			}
+		}
 	}
 </script>
